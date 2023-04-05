d497b477af5ee38ba701a9f75365df881263bc88
iamsaief <saiefalemon@gmail.com>
2023-04-06 00:03:07 +0600

    work, project, testimonial

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ExperienceCard = ({ experience }) => {
+	return (
+		<VerticalTimelineElement
+			contentStyle={{ background: '#1d1836', color: '#fff' }}
+			contentArrowStyle={{ borderRight: '7px solid #1d1836' }}
+			date={experience.date}
+			iconStyle={{ background: experience.iconBg }}
+			icon={
+				<div className="flex justify-center items-center w-full h-full">
+					<img src={experience.icon} alt={experience.company_name} className="w-[60%] h-[60%] object-contain" />
+				</div>
+			}
+		>
+			<div>
+				<h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+				<p className="text-white/50 text-[16px] font-semibold !m-0">{experience.company_name}</p>
+				<ul className="mt-5 list-disc ml-5 space-y-2">
+					{experience.points.map((point, index) => (
+						<li key={`experience-point-${index}`} className="text-white/80 text-[14px]pl-1 ">
+							{point}
+						</li>
+					))}
+				</ul>
+			</div>
+		</VerticalTimelineElement>
+	);
+};
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={`${styles.sectionSubText}`}>Wha I have done so far</p>
+				<h2 className={`${styles.sectionHeadText}`}>Work Experience.</h2>
+			</motion.div>
 
-export default Experience
+			<div className="mt-20 flex flex-col">
+				<VerticalTimeline>
+					{experiences.map((experience, index) => (
+						<ExperienceCard key={index} experience={experience} />
+					))}
+				</VerticalTimeline>
+			</div>
+		</>
+	);
+};
+
+export default SectionWrapper(Experience, 'work');
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
+import { projects } from '../constants';
+import { github } from '../assets';
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+	return (
+		<motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+			<Tilt options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+				<div className="relative w-full h-[230px]">
+					<img src={image} alt="name" className="w-full h-full object-cover rounded-2xl" />
+					<div className="absolute inset-0 flex justify-end m-3 img-card_hover">
+						<div
+							className="black-gradient w-10 h-10 rounded-full cursor-pointer flex items-center justify-center"
+							onClick={() => window.open(source_code_link, '_blank')}
+						>
+							<img src={github} className="w-1/2 h-1/2 object-contain" />
+						</div>
+					</div>
+				</div>
+				<div className="mt-5">
+					<h3 className="text-white font-bold text-[24px]">{name}</h3>
+					<p className="mt-2 text-secondary text-[14px] line-clamp-4 hover:line-clamp-none">{description}</p>
+				</div>
+				<div className="mt-4 flex flex-wrap gap-2">
+					{tags.map((tag) => (
+						<p key={tag.name} className={`text-[14px] ${tag.color}`}>
+							#{tag.name}
+						</p>
+					))}
+				</div>
+			</Tilt>
+		</motion.div>
+	);
+};
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={`${styles.sectionSubText}`}>My works</p>
+				<h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+			</motion.div>
+			<div className="w-full flex">
+				<motion.p
+					variants={fadeIn('', '', 0.1, 1)}
+					className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+				>
+					I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks
+					like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create
+					efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring
+					your ideas to life!
+				</motion.p>
+			</div>
 
-export default Works
+			<div className="mt-20 flex flex-wrap gap-7">
+				{projects.map((project, index) => (
+					<ProjectCard key={`project-${index}`} index={index} {...project} />
+				))}
+			</div>
+			<div className="mt-10 text-center">
+				<a
+					href="https://github.com/iamsaief"
+					target="_blank"
+					className="px-6 py-2 font-sm text-base  bg-tertiary text-white rounded-full shadow-sm"
+				>
+					View more
+				</a>
+			</div>
+		</>
+	);
+};
+
+export default SectionWrapper(Works, '');
